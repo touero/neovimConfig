37e6cd2a0159dfb594ce298ae58c0f45295ff2fa
touero <touer0018@gmail.com>
2024-02-03 00:48:02 +0800

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", {})
+vim.api.nvim_set_keymap("i", "jkf", "<Esc>:wq<CR>", { silent = true })
+
+vim.api.nvim_set_keymap("n", ";", ":", {})
+vim.api.nvim_set_keymap("x", ";", ":", { expr = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ">>", {})
+vim.api.nvim_set_keymap("n", "<S-Tab>", "<<", {})
+vim.api.nvim_set_keymap("n", "<CR>", ":normal o<CR>", {})
+vim.api.nvim_set_keymap("n", "<S-Enter>", ":normal O<CR>", {})
+
+vim.api.nvim_set_keymap("v", "<Tab>", ">gc", {})
+vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", {})
